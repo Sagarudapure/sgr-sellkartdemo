b5e35e74a6c215951190f73e2b6b5850be8c83fb
Sagar <sagarudapure100@gmail.com>
2025-03-01 16:27:08 +0530

    test

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+// import Navbar from './layouts/Navbar'
+
+
+function App() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      {/* <Navbar/> */}
+    </div>
+  )
+}
+
+export default App
